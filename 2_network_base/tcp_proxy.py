d4c3bd86943cecd76bcdf5642ea57bb5fec01c74
sentient0being <39542205+sentient0being@users.noreply.github.com>
2018-08-28 16:42:31 +0800

    Add files via upload

--- 2_network_base/tcp_proxy.py
+++ 2_network_base/tcp_proxy.py
@@ -3,10 +3,11 @@ import socket
 import threading
 
 def hexdump(src,length=16):
-	result = []
+	
 	digits = 4 if isinstance(src,unicode) else 2
 
 	for i in xrange(0,len(src),length):
+		result = []
 		s = src[i:i+length]
 		hexa = b' '.join(["%0*X" % (digits,ord(x)) for x in s])
 		text = b''.join([x if 0x20 <= ord(x) <0x7F else b'.' for x in s])
